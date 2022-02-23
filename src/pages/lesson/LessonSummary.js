be9e2b5c2f73a3640e6b3b99fab6fbc2ce77d525
dkosznik <52452148+dkosznik@users.noreply.github.com>
2022-02-23 19:11:38 +0000

    styles and all words logic updated

--- src/pages/lesson/LessonSummary.js
+++ src/pages/lesson/LessonSummary.js
@@ -23,46 +23,141 @@ export default function LessonSummary({lesson}) {
     const [anser8, setAnser8] = useState('')
     const [anser9, setAnser9] = useState('')
     const [anser10, setAnser10] = useState('')
+    const [correct, setCorrect] = useState('disabled')
+    const [wrong, setWrong] =useState('disabled')
+    const [displayStyles, setDisplayStyles] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(anser1, anser2)
+
+        setDisplayStyles('active')
+
+        if (anser1 === lesson.word1) {
+            setCorrect('active')
+        }
+        if (anser1 !== lesson.word1) {
+            setWrong('active')
+        }
+        if (anser2 === lesson.word2) {
+            setCorrect('active')
+        }
+        if (anser2 !== lesson.word2) {
+            setWrong('active')
+        }
+        if (anser3 === lesson.word3) {
+            setCorrect('active')
+        }
+        if (anser3 !== lesson.word3) {
+            setWrong('active')
+        }
+        if (anser4 === lesson.word4) {
+            setCorrect('active')
+        }
+        if (anser4 !== lesson.word4) {
+            setWrong('active')
+        }
+        if (anser5 === lesson.word5) {
+            setCorrect('active')
+        }
+        if (anser5 !== lesson.word5) {
+            setWrong('active')
+        }
+        if (anser6 === lesson.word6) {
+            setCorrect('active')
+        }
+        if (anser6 !== lesson.word6) {
+            setWrong('active')
+        }
+        if (anser7 === lesson.word7) {
+            setCorrect('active')
+        }
+        if (anser7 !== lesson.word7) {
+            setWrong('active')
+        }
+        if (anser8 === lesson.word8) {
+            setCorrect('active')
+        }
+        if (anser8 !== lesson.word8) {
+            setWrong('active')
+        }
+        if (anser9 === lesson.word9) {
+            setCorrect('active')
+        }
+        if (anser9 !== lesson.word9) {
+            setWrong('active')
+        }
+        if (anser10 === lesson.word10) {
+            setCorrect('active')
+        }
+        if (anser10 !== lesson.word10) {
+            setWrong('active')
+        }
+        
+        
+        
     }
 
   return (
     <form onSubmit={handleSubmit}>
-        <div className='words'>
-        <div className='speechButton'>
-            <Speech 
-                textAsButton={true}    
-                displayText="Play" 
-                text={lesson.word1}
-                />
+            <div className={displayStyles}>
+                    <Speech 
+                        textAsButton={true}    
+                        displayText="Play" 
+                        text={lesson.word1}
+                        />
+                    <Input placeholder="Write it down" inputProps={ariaLabel} 
+                        required
+                        type="text"
+                        onChange={(e) => setAnser1(e.target.value)}
+                        value={anser1}
+                    />
+                    {lesson.word1 === anser1
+                        ? 
+                        <Alert className={correct} variant="filled" severity="success">
+                            Correct
+                        </Alert>
+                        :
+                        <>
+                            <Alert className={wrong} variant="filled" severity="error">
+                                Wrong
+                            </Alert>
+                            <Alert className={wrong} severity="info">
+                                {lesson.word1}
+                            </Alert>
+                        </>
+                        
+                    }
                 </div>
-            <Input placeholder="Write it down" inputProps={ariaLabel} 
-                required
-                type="text"
-                onChange={(e) => setAnser1(e.target.value)}
-                value={anser1}
-            />
-            <Alert className='disabled' variant="filled" severity="error">
-                This is an error
-            </Alert>
-        </div>
-        <div>
-            <Speech 
-                textAsButton={true}    
-                displayText="Play" 
-                text={lesson.word2}
+            <div className={displayStyles}>
+                <Speech 
+                    textAsButton={true}    
+                    displayText="Play" 
+                    text={lesson.word2}
+                    />
+                <Input placeholder="Write it down" inputProps={ariaLabel} 
+                    required
+                    type="text"
+                    onChange={(e) => setAnser2(e.target.value)}
+                    value={anser2}
                 />
-            <Input placeholder="Write it down" inputProps={ariaLabel} 
-                required
-                type="text"
-                onChange={(e) => setAnser2(e.target.value)}
-                value={anser2}
-            />
+                {lesson.word2 === anser2
+                    ? 
+                    <Alert className={correct} variant="filled" severity="success">
+                        Correct
+                    </Alert>
+                    :
+                    <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word2}
+                        </Alert>
+                    </>
+                    
+                }
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -74,8 +169,24 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser3(e.target.value)}
                 value={anser3}
             />
+            {lesson.word3 === anser3
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word3}
+                        </Alert>
+                    </> 
+            }
+            
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -87,8 +198,23 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser4(e.target.value)}
                 value={anser4}
             />
+            {lesson.word4 === anser4
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word4}
+                        </Alert>
+                    </> 
+            }
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -100,8 +226,23 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser5(e.target.value)}
                 value={anser5}
             />
+            {lesson.word5 === anser5
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word5}
+                        </Alert>
+                    </> 
+            }
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -113,8 +254,23 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser6(e.target.value)}
                 value={anser6}
             />
+            {lesson.word6 === anser6
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word6}
+                        </Alert>
+                    </> 
+            }
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -126,8 +282,23 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser7(e.target.value)}
                 value={anser7}
             />
+            {lesson.word7 === anser7
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word7}
+                        </Alert>
+                    </> 
+            }
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -139,8 +310,23 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser8(e.target.value)}
                 value={anser8}
             />
+            {lesson.word8 === anser8
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word8}
+                        </Alert>
+                    </> 
+            }
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -152,8 +338,23 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser9(e.target.value)}
                 value={anser9}
             />
+            {lesson.word9 === anser9
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word9}
+                        </Alert>
+                    </> 
+            }
         </div>
-        <div>
+        <div className={displayStyles}>
             <Speech 
                 textAsButton={true}    
                 displayText="Play" 
@@ -165,6 +366,21 @@ export default function LessonSummary({lesson}) {
                 onChange={(e) => setAnser10(e.target.value)}
                 value={anser10}
             />
+            {lesson.word10 === anser10
+                ? 
+                <Alert className={correct} variant="filled" severity="success">
+                    Correct
+                </Alert>
+                :
+                <>
+                        <Alert className={wrong} variant="filled" severity="error">
+                            Wrong
+                        </Alert>
+                        <Alert className={wrong} severity="info">
+                            {lesson.word10}
+                        </Alert>
+                    </> 
+            }
         </div>
         <button className="btn">Check</button>
         
